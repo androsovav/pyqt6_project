4afd3e3f17a54280dfdacfec7473000fabb55c60
androsovav <androsov.av@internet.ru>
2024-12-27 16:44:32 +0300

    Пройдены несколько этапов обучения PyQt6

--- venv/4.py
+++ venv/4.py
@@ -1,7 +1,10 @@
 import sys
+
+from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
+# Подкласс QMainWindow для настройки главного окна приложения
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -9,14 +12,11 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
+
+        self.setFixedSize(QSize(400, 300))
 
         # Устанавливаем центральный виджет Window.
         self.setCentralWidget(button)
-
-    def the_button_was_clicked(self):
-        print("Clicked!")
 
 
 app = QApplication(sys.argv)
